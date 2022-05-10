7ccf631d55e7b9aeed489b5cb01506f7450bdc83
isaac <isaac.marquetti@gmail.com>
2022-05-09 23:27:25 -0300

    Listar tarefas pendentes
    close #4

--- webdev/tarefas/views.py
+++ webdev/tarefas/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from django.urls import reverse
 
 from webdev.tarefas.forms import TarefaNovaForm
+from webdev.tarefas.models import Tarefa
 
 
 def home(request):
@@ -12,5 +13,7 @@ def home(request):
             form.save()
             return HttpResponseRedirect(reverse('tarefas:home'))
         else:
-            return render(request, 'tarefas/home.html', {'form': form}, status=400)
-    return render(request, 'tarefas/home.html')
+            tarefas_pendentes = Tarefa.objects.filter(feita=False).all()
+            return render(request, 'tarefas/home.html', {'form': form, 'tarefas_pendentes': tarefas_pendentes}, status=400)
+    tarefas_pendentes = Tarefa.objects.filter(feita=False).all()
+    return render(request, 'tarefas/home.html', {'tarefas_pendentes': tarefas_pendentes})
